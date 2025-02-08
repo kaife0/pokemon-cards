55cde6cdf3d7ace6f522b7ed17f56e8e1558425b
Md Kaif <163404336+kaife0@users.noreply.github.com>
2025-02-08 11:27:27 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -22,7 +22,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-    },
+    }, 
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
